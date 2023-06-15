8c97f5563069dfc4967587491b87b2ffe53a6bc2
Avneesh <iavneeshsing@gmail.com>
2023-06-15 18:14:36 +0530

    Order Agnostic Binary Search Implemented in previous Binary Search code.

--- Binary_Search/BinarySearch.java
+++ Binary_Search/BinarySearch.java
@@ -2,8 +2,8 @@ package Binary_Search;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] arr = {2,4,6,9,11,12,14,20,36,48};
-        int target = 4;
+        int[] arr = {25,22,19,10,7,6,2,1,0};
+        int target = 22;
         if(position(arr, target) == -1)
         System.out.println("Element is not present in Array");
        else
@@ -14,17 +14,28 @@ public class BinarySearch {
       static int position(int[] arr, int target){
         int start = 0;
         int end = arr.length-1;
+
+        //checking whether the Array is sorted in Ascending or Descending order
+        boolean isAsc = arr[start] < arr[end];
         
         while(start<=end){
             int mid = start+(end-start)/2;
             if(target == arr[mid])  //checking if element is present at middle of the array
                 return mid;
 
-                //checking in left side of array
+            if(isAsc){
+
              if(target< arr[mid])
                 end = mid-1;
             else
                 start = mid+1;
+            }
+            else {
+                if(target > arr[mid])
+                   end = mid-1;
+            else
+                start = mid+1;
+            }
       }
 
     return -1;
