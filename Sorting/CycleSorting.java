c49ce6ef87b4e027eb236cfb4209580ab002364b
Avneesh <iavneeshsing@gmail.com>
2023-07-25 15:35:31 +0530

    Cycle Sorting Algorithm is implemented.

---
+++ Sorting/CycleSorting.java
@@ -0,0 +1,32 @@
+//Implementing Basic Cycle sorting algorithm
+package Sorting;
+
+import java.util.Arrays;
+
+public class CycleSorting {
+    public static void main(String[] args) {
+        int[] arr = {6,5,4,3,2,1};
+        sorting(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void sorting(int[] arr){
+         int i = 0;
+        while(i < arr.length){
+            int correct = arr[i] - 1;
+
+            if(arr[i] != arr[correct]){
+                swap(arr, i, correct);
+            }
+            else{
+                i++;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
