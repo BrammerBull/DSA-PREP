36f944c8f8351cd3bab066ddf69cbc4d31c7cfc7
Avneesh <iavneeshsing@gmail.com>
2023-07-31 20:35:47 +0530

    Recursion Started and Solved two basic questions

---
+++ Recursion/fibonacci.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class fibonacci {
+    public static void main(String[] args) {
+        System.out.println(fibo(1));
+    }
+
+    static int fibo(int n){
+      //base condition
+      if(n < 2){
+        return n;
+      }
+
+        return fibo(n-1) + fibo(n-2);
+    }
+}
